56127dcc6f2e498cca52dbcc2c77ffdc8a0efe49
Cynthia Wu <cynthia21wu@gmail.com>
2021-02-11 12:35:42 -0800

    Fixing issues with react router dom

--- models/book.js
+++ models/book.js
@@ -1,32 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bookSchema = new mongoose.Schema({
-// title of the book as returned by Google API
-  title: {
-    type: String,
-    required: true
-  },
-// author(s) of the book as returned by Google API
-  authors: {
-    type: [String],
-    default: ""
-  },
-// description of the book as returned by Google API
-  description: {
-    type: String,
-    default: ""
-  },
-// the book's thumbnail image as returned by Google API
-  image: {
-    type: String,
-    default: ""
-  },
-  // the book's information of link as returned by Google API
-  link: {
-    type: String,
-    default: "",
-  }
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  synopsis: String,
+  date: { type: Date, default: Date.now }
 });
 
 const Book = mongoose.model("Book", bookSchema);

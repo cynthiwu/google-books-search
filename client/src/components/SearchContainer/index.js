3887e8990e2e96521eeb16132ac1af7fc276d827
Cynthia Wu <cynthia21wu@gmail.com>
2021-02-11 18:26:10 -0800

    Adding the google books API back

--- client/src/components/SearchContainer/index.js
+++ client/src/components/SearchContainer/index.js
@@ -2,9 +2,29 @@ import React from "react";
 import "./style.css";
 import SearchForm from "../SearchForm";
 import SearchCard from "../SearchCard";
+import SearchContext from "../../utils/SearchContext";
 
 
 function SearchContainer() {
+
+    const [query, setQuery] = useState("");
+    const [result, setResult] = useState([]);
+
+    function handleSearch(event) {
+        event.preventDefault();
+
+        let query = event.target.search.value;
+        
+        if (formObject.title && formObject.author) {
+          API.saveBook({
+            title: formObject.title,
+            author: formObject.author,
+            synopsis: formObject.synopsis
+          })
+            .then(res => loadBooks())
+            .catch(err => console.log(err));
+        }
+      };
 
   return (
     
--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,17 +1,15 @@
 import React from "react";
 import "./style.css";
-import SearchContext from "../../utils/SearchContext";
+
 function Search() {
 
-    const [query, setQuery] = useState("");
-    const [result, setResult] = useState([]);
 
   return (
   
     <form className="form"> 
         <label className="form-title">Book Search</label>
             <div className="form-inline">
-                <input type="text" className="form-control" id="book-search"  placeholder="Enter book title" />
+                <input type="search" name="search" className="form-control" id="search"  placeholder="Enter book title" />
                 <button type="submit" className="btn btn-primary font-weight-bold">Search</button>
             </div>
         

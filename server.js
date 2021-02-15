e5bdf54499efd673aebdae82592aa191260d90f9
Cynthia Wu <cynthia21wu@gmail.com>
2021-02-14 16:23:36 -0800

    Preparing to deploy

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // API routes
